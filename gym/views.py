f5b40ffa2c0c48619321b26477890ad4260cfc22
minwoo0419 <ghdl464@naver.com>
2023-08-05 00:41:52 +0900

    Feat : 좌표변환 함수 추가

--- gym/views.py
+++ gym/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from urllib.parse import urlparse
+import requests, json
 
-# Create your views here.
+
+def get_location(address):
+    url = "https://dapi.kakao.com/v2/local/search/address.json?query=" + address
+    headers = {"Authorization": "KakaoAK 6556fdbf5f3e592092dbd5678bb76f64"}
+    api_json = json.loads(str(requests.get(url, headers=headers).text))
+    address = api_json["documents"][0]["address"]
+    latitude = float(address["x"])
+    longitude = float(address["y"])
+    return (latitude, longitude)
+
+
+print(get_location("대구광역시 달서구 상화로 86"))
