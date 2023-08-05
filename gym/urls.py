267476581a356bb19041a2fddb74285f1f2c7697
minwoo0419 <ghdl464@naver.com>
2023-08-05 15:53:45 +0900

    feat : gym, gymreport기능 구현

--- gym/urls.py
+++ gym/urls.py
@@ -1,7 +1,12 @@
-from django.urls import path
+from django.urls import include, path
 from .views import *
-from . import views
+from rest_framework import routers
+
 app_name = "gym"
+
+default_router = routers.SimpleRouter()
+default_router.register("gyms", GymViewSet, basename="gyms")
+
 urlpatterns = [
-    
+    path("api/", include(default_router.urls)),
 ]
