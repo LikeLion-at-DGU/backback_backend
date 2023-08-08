5685234f477193084eb732ad1bbfcad8b0a37da5
0seoYun <yys020819@gmail.com>
2023-08-08 13:38:49 +0900

    feat:Post API 작업

---
+++ community/serializers.py
@@ -0,0 +1,101 @@
+from rest_framework import serializers
+from .models import *
+
+
+class PostImageSerializer(serializers.ModelSerializer):
+    image = serializers.ImageField(use_url=True)
+
+    class Meta:
+        model = PostImage
+        fields = "__all__"
+
+
+class ExerciseSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Exercise
+        fields = "__all__"
+
+
+class PurposeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Purpose
+        fields = "__all__"
+
+
+class PostListSerializer(serializers.ModelSerializer):
+    # likes_cnt = serializers.SerializerMethodField()
+    # comments_cnt = serializers.SerializerMethodField()
+    content = serializers.CharField(write_only=True)
+    content_short = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Post
+        fields = [
+            "id",
+            "created_at",
+            "updated_at",
+            # "writer",
+            "title",
+            "content",
+            "content_short",
+            # "likes_cnt",
+            # "comments_cnt",
+        ]
+
+    def create(self, validated_data):
+        instance = Post.objects.create(**validated_data)
+        image_set = self.context["request"].FILES
+        for image_data in image_set.getlist("image"):
+            PostImage.objects.create(post=instance, image=image_data)
+        return instance
+
+    def get_content_short(self, obj):
+        return obj.content[:5] + "..." if len(obj.content) > 5 else obj.content
+
+    # def get_comments_cnt(self, instance):
+    #     return instance.comments.count()
+
+
+class PostDetailSerializer(serializers.ModelSerializer):
+    purposes = PurposeSerializer(many=True)
+    exercises = ExerciseSerializer(many=True)
+    images = serializers.SerializerMethodField()
+    is_clipped = serializers.SerializerMethodField()
+    # comments_cnt = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Post
+        fields = [
+            "id",
+            "created_at",
+            "updated_at",
+            # "writer",
+            "purposes",
+            "exercises",
+            "title",
+            "images",
+            "content",
+            # "likes_cnt",
+            # "comments_cnt",
+            # "is_liked",
+            "is_clipped",
+        ]
+
+    def get_images(self, obj):
+        image = obj.postimages.all()
+        return PostImageSerializer(instance=image, many=True, context=self.context).data
+
+    def get_is_clipped(self, obj):
+        user = self.context["request"].user
+        return Scrap.objects.filter(user=user, post=obj).exists()
+
+    # def get_comments_cnt(self, instance):
+    #     return instance.comments.count()
+
+
+class ScrapSerializer(serializers.ModelSerializer):
+    post = PostListSerializer()
+
+    class Meta:
+        model = Scrap
+        fields = "__all__"
