52cb3ba73274ea745fa12c0468cbfde0757df41f
jihyunniiii <jita1125@naver.com>
2023-08-09 17:47:08 +0900

    feature/21: WriterSerializer 수정

--- core/serializers.py
+++ core/serializers.py
@@ -1,9 +1,12 @@
 from rest_framework import serializers
 from .models import *
+from accounts.models import *
 
 
 class WriterSerializer(serializers.ModelSerializer):
+    profile_id = serializers.IntegerField(source="id")
+
     class Meta:
-        model = User
-        fields = ["id", "nickname", "type"]
+        model = Profile
+        fields = ["profile_id", "nickname", "type"]
         read_only_field = fields
