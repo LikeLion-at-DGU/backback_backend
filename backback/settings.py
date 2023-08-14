0a2fc154b39ab9b6290096a3ce61176ac051eb40
tkdwns414 <sangjune2000@naver.com>
2023-08-14 18:09:07 +0900

    db settings

--- backback/settings.py
+++ backback/settings.py
@@ -132,9 +132,14 @@ WSGI_APPLICATION = "backback.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": os.getenv("DATABASE_NAME"),
+        "USER": os.getenv("DATABASE_USER"),
+        "PASSWORD": os.getenv("DATABASE_PASSWORD"),
+        "HOST": os.getenv("DATABASE_HOST"),
+        "PORT": os.getenv("DATABASE_PORT"),
+        "OPTIONS": {"charset": "utf8mb4"},
+    },
 }
 
 
