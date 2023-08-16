002912b96da4b0e09a0e1031de382370d96eb36b
tkdwns414 <sangjune2000@naver.com>
2023-08-16 21:06:13 +0900

    add all models to admin

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 
-from accounts.models import Profile
+from accounts.models import Profile, ProfileReport
 
 admin.site.register(Profile)
+admin.site.register(ProfileReport)
